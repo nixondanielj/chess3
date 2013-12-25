c61a977029131c4715bf7f6664fb09e10bed6aea
Dan <daniel.nixon@onshoreoutsourcing.com>
2013-12-24 19:59:22 -0500

    added dimodule to services project

---
+++ Services/DIModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    class DIModule : Ninject.Modules.NinjectModule
+    {
+        public override void Load()
+        {
+            
+        }
+    }
+}
