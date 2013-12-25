aedcaaefe94db621664f7ea127b3385045afeec4
Dan <daniel.nixon@onshoreoutsourcing.com>
2013-12-24 21:37:59 -0500

    added board rendering

---
+++ ChessAPI/Scripts/chess_renderer.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    board = $('.gameboard');
+    drawBoard();
+});
+var board, darkSquare;
+
+function drawBoard() {
+    board.children(".boardrow").each(function (elI, el) {
+        var row = $(el);
+        for (var i = 0; i < 8; i++) {
+            addSquareToRow(elI % 2 == 0, row);
+        }
+    });
+}
+
+function addSquareToRow(evenRow, row) {
+    var square = $('<div>0</div>')
+        .addClass(darkSquare ^ evenRow ? "dark" : "light")
+        .addClass("square");
+    darkSquare = !darkSquare;
+    row.append(square);
+}
