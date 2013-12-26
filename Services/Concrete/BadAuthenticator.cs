02640bd350dbc03fe983f4ea2bee7071f3062543
Dan <daniel.nixon@onshoreoutsourcing.com>
2013-12-26 17:23:22 -0500

    added mock authorization service

---
+++ Services/Concrete/BadAuthenticator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Concrete
+{
+    public class BadAuthenticator : IAuthorizationService
+    {
+        public bool Authenticate(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Authenticate(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/IAuthorizationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface IAuthorizationService
+    {
+        public bool Authenticate(string key);
+        public string Authenticate(string email, string password);
+    }
+}
