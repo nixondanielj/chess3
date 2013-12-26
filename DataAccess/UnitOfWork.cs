62a9307db0180e1b2d5f938ed9e1b0043f2c0ea4
Dan <daniel.nixon@onshoreoutsourcing.com>
2013-12-26 15:28:57 -0500

    added repository and uow

---
+++ DataAccess/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtilityAbstractions;
+
+namespace DataAccess
+{
+    public class UnitOfWork : IDisposable
+    {
+        private GameContext db { get; set; }
+        private ILogger log { get; set; }
+        public UnitOfWork(ILogger log)
+        {
+            this.db = new GameContext();
+            this.log = log;
+        }
+        public Repository<T> GetRepository<T>() where T: class
+        {
+            return new Repository<T>(db);
+        }
+
+        public void Dispose()
+        {
+
+            db.Dispose();
+        }
+    }
+}
