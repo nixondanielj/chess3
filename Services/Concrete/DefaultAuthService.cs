6f59ab0ea62fa7ed768d8c8ed359240cd236e79e
Dan <daniel.nixon@onshoreoutsourcing.com>
2013-12-26 17:31:29 -0500

    added authvm

--- Services/Concrete/DefaultAuthService.cs
+++ Services/Concrete/DefaultAuthService.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccess.Model;
+using DataAccess;
+using Services.Models;
 
 namespace Services.Concrete
 {
     public class DefaultAuthService : IAuthorizationService
     {
-        public int Authenticate(string key)
+        public AuthenticationVM Authenticate(string key)
         {
-
+            using (var uow = new UnitOfWork())
+            {
+            }
         }
 
-        public string Authenticate(string email, string password)
+        public string Authenticate(AuthenticationVM model)
         {
             return "a";
         }
