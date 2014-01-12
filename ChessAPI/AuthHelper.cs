9b5e5b29a92bf4dd802b2a487cd9bed81124165e
dan <nixon.daniel.j@gmail.com>
2014-01-12 11:15:57 -0500

    added authhelper

---
+++ ChessAPI/AuthHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Services.Models;
+using System.Net.Http;
+using System.Web.Http;
+using System.Net;
+using Services;
+
+namespace ChessAPI
+{
+    public class AuthHelper
+    {
+        private readonly IAuthorizationService authService;
+
+        public AuthHelper(IAuthorizationService authService)
+        {
+
+        }
+
+        public AuthenticationVM Authenticate(HttpRequestMessage request)
+        {
+            AuthenticationVM user = null;
+            string sessionId = GetSessionId(request);
+            if (sessionId != null)
+            {
+                user = authService.Authenticate(sessionId);
+            }
+            if (user == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.Forbidden);
+            }
+            return user;
+        }
+
+        public string GetSessionId(HttpRequestMessage request)
+        {
+            string sessionId = null;
+            var cookie = request.Headers.GetCookies("sessionId").FirstOrDefault();
+            if (cookie != null)
+            {
+                sessionId = cookie["sessionId"].Value;
+            }
+            return sessionId;
+        }
+
+        public void SetSessionCookie(HttpRequestMessage request, HttpResponseMessage response, string sessionKey)
+        {
+            var cookie = new CookieHeaderValue("sessionId", sessionKey);
+            cookie.Expires = DateTimeOffset.Now.AddMinutes(15);
+            cookie.Path = "/";
+            response.Headers.AddCookies(new CookieHeaderValue[] { cookie });
+        }
+    }
+}
