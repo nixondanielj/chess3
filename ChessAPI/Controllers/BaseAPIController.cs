9604146426c69578e462090a13b047c91b8c58b3
dan <nixon.daniel.j@gmail.com>
2014-01-12 11:49:05 -0500

    added BaseAPIController

---
+++ ChessAPI/Controllers/BaseAPIController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace ChessAPI.Controllers
+{
+    public class BaseAPIController : ApiController
+    {
+        public HttpRequestBase Request { get; set; }
+        public HttpResponseBase Response { get; set; }
+
+        public BaseAPIController()
+        {
+            var context = new HttpContextWrapper(HttpContext.Current);
+            this.Request = context.Request;
+            this.Response = context.Response;
+        }
+    }
+}
